7780359a0d2247ed6be7d34419869a2b30ebb660
maxime_chostak <mchostak@yahoo.fr>
2024-04-23 16:56:27 +0200

    creation banniere evenements

---
+++ frontendV2/src/views/EvenementView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="header-banner header-banner-img">
+        <div class="header-gradiant"></div>
+    </div>
+</template>
+<script>
+</script>
+
+<style>
+.header-banner.header-banner-img
+{
+    width: 100vw;
+    height: 430px;
+    position: relative;
+    margin-left: -50vw;
+    left: 50%;
+}
+
+.header-banner-img
+{
+    background-image: url('@/assets/thumbnail_atelier.jpg');
+    background-size: cover;
+    background-position: center;
+}
+</style>
